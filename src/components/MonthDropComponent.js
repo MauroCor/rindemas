99e87b8d04baed14b33cde5e2cc33221740b0ec7
Mauro Corrales <mauro.corrales@fravega.com.ar>
2024-11-12 13:45:25 -0300

    fix monthdropcomponent useefect

--- src/components/MonthDropComponent.js
+++ src/components/MonthDropComponent.js
@@ -1,19 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const MonthDropComponent = ({ value, onChange, type }) => {
+    const [initialValueSet, setInitialValueSet] = useState(false);
+
     useEffect(() => {
-        if (type === "Desde" && !value) {
+        if (type === "Desde" && !initialValueSet) {
             const currentDate = new Date();
             const formattedDate = `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(2, '0')}`;
             onChange(formattedDate);
-        }
-
-        if (type === "DesdeTarj" && !value) {
+            setInitialValueSet(true);
+        } else if (type === "DesdeTarj" && !initialValueSet) {
             const currentDate = new Date();
             const formattedDate = `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 2).padStart(2, '0')}`;
             onChange(formattedDate);
+            setInitialValueSet(true);
         }
-    }, [type, value, onChange]);
+    }, [type, initialValueSet, onChange]);
+
+    useEffect(() => {
+        setInitialValueSet(false);
+    }, [type]);
 
     return (
         <div className="text-center mb-2 relative">
