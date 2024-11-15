2db2b29b55f9b1b96f48030514546f2229007c65
Mauro Corrales <mauro.corrales@fravega.com.ar>
2024-11-15 16:15:41 -0300

    fix center alight

--- src/components/DropdownComponent.js
+++ src/components/DropdownComponent.js
@@ -2,20 +2,22 @@ const DropdownComponent = ({ value, onChange }) => {
   return (
     <div className="flex flex-col text-center">
       <label className="text-xs text-left mb-1 ml-11 text-white">Cuotas</label>
-      <select
-        value={value}
-        onChange={onChange}
-        className="bg-gray-700 mx-10 py-2 rounded-lg text-white text-center"
-      >
-        <option value="1" className="text-center">1 cuota</option>
-        {[3, 6, 9, 12, 18].map((cuota) => (
-          <option key={cuota} value={cuota} className="text-center">
-            {cuota} cuotas
-          </option>
-        ))}
-      </select>
+      <div className="relative mx-10">
+        <select
+          value={value}
+          onChange={onChange}
+          className="bg-gray-700 py-2 w-full rounded-lg text-white text-lg appearance-none text-center text-indent-[50%]"
+          >
+          <option value="1">1 cuota</option>
+          {[3, 6, 9, 12, 18].map((cuota) => (
+            <option key={cuota} value={cuota}>
+              {cuota} cuotas
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
-  );
-};
+  )
+};  
 
 export default DropdownComponent;
