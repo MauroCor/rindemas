1252bf6c5ebc8ebf49b9d9d1156cb4371065c9ab
Mauro Corrales <mauro.corrales@fravega.com.ar>
2024-11-18 23:19:25 -0300

    istokenvalid

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,9 +1,21 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const AuthContext = createContext();
 
+const isTokenValid = (token) => {
+  try {
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    return payload.exp * 1000 > Date.now(); // Verifica si el token no ha expirado
+  } catch (e) {
+    return false;
+  }
+};
+
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(localStorage.getItem('access'));
+  const [token, setToken] = useState(() => {
+    const storedToken = localStorage.getItem('token');
+    return storedToken && isTokenValid(storedToken) ? storedToken : null;
+  });
 
   const login = (jwtToken) => {
     localStorage.setItem('token', jwtToken);
@@ -14,6 +26,12 @@ export const AuthProvider = ({ children }) => {
     localStorage.removeItem('token');
     setToken(null);
   };
+
+  useEffect(() => {
+    if (token && !isTokenValid(token)) {
+      logout();
+    }
+  }, [token]);
 
   return (
     <AuthContext.Provider value={{ token, login, logout }}>
