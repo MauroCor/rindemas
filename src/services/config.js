b0c404f8a7d140c7729f614b7d546f0de4179ac0
Mauro Corrales <mauro.corrales@fravega.com.ar>
2024-11-18 23:20:38 -0300

    ch config

--- src/services/config.js
+++ src/services/config.js
@@ -1,2 +1,2 @@
-export const base_url = 'http://127.0.0.1:5050';
-// export const base_url = 'https://stagemoneyback.pythonanywhere.com';
+// export const base_url = 'http://127.0.0.1:5050';
+export const base_url = 'https://stagemoneyback.pythonanywhere.com';
